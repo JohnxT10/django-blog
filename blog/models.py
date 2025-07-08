7042d06ab8361e80d84ebb048b23e55d7b9897e1
JohnxT10 <johnxthompson01@gmail.com>
2025-07-08 17:40:05 +0100

    blog url

--- blog/models.py
+++ blog/models.py
@@ -34,7 +34,6 @@ class Comment(models.Model):
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
 
-
     class Meta:
         ordering = ["created_on"]
 
